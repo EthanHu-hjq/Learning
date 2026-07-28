ac778568727628e8346c0dbb27ba6200821df104
Ethan <Ethan.Hu@tymphany.com>
2026-07-28 17:38:33 +0800

    增加流程控制练习，锻炼python语法

---
+++ 基础练习/流程控制练习.py
@@ -0,0 +1,16 @@
+# 需求：根据输入的用户名密码执行登录操作，具体要求如下：
+# 1.正确的用户名密码为 admin/123456 ， zhangsan/234567 , hjq/999999
+# 2.输入用户名和密码进行登录，直到登录成功，程序结束运行，否则继续输入
+# 3.输入的用户密码不能为空
+# 4.登录成功输出“登录成功”否则输出“用户名或密码错误，请重新输入"
+
+account1,account2,account3  = 'admin','zhangsan','hjq'
+password1,password2,password3 = '123456','234567','999999'
+
+is_ok = False # 登录成功标志位
+while not is_ok:
+    in_account =  input('请输入用户名：')
+    in_password = input('请输入密码：')
+
+
+
