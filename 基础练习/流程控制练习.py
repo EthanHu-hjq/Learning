ddc7272d3570bf9069de4ac330c5b289fc6b8af9
Ethan <Ethan.Hu@tymphany.com>
2026-07-29 17:35:23 +0800

    完成流程控制练习，增加random语句文件

--- 基础练习/流程控制练习.py
+++ 基础练习/流程控制练习.py
@@ -4,13 +4,37 @@
 # 3.输入的用户密码不能为空
 # 4.登录成功输出“登录成功”否则输出“用户名或密码错误，请重新输入"
 
-account1,account2,account3  = 'admin','zhangsan','hjq'
-password1,password2,password3 = '123456','234567','999999'
-
-is_ok = False # 登录成功标志位
-while not is_ok:
-    in_account =  input('请输入用户名：')
-    in_password = input('请输入密码：')
-
-
-
+# 1.传统写法
+# account1,account2,account3  = 'admin','zhangsan','hjq'
+# password1,password2,password3 = '123456','234567','999999'
+#
+# while True:
+#     in_account =  input('请输入用户名：')
+#     in_password = input('请输入密码：')
+#     if in_account == account1 and in_password == password1:
+#         print('登录成功')
+#         break
+#     elif in_account == account2 and in_password == password2:
+#         print('ok')
+#         break
+#     elif in_account == account3 and in_password == password3:
+#         print('ok')
+#         break
+#     else:
+#         print('ng')
+# ------------------------------------------------------------
+# 2.使用字典简化代码
+# users = {
+#     'admin':'123456',
+#     'zhangsan':'234567',
+#     'hjq':'999999'
+# }
+#
+# while True:
+#     input_user = input('please input your name:')
+#     input_password = input('please input your password:')
+#     if input_user in users and input_password == users[input_user]:
+#         print('OK')
+#         break
+#     else:
+#         print('Wrong username or password')
